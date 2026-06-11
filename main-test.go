39bc05b336622be9c47a3e8fea87b79606b6ab50
Jasgitcodes <jamesenelaw@gmail.com>
2026-06-11 04:17:04 +0100

    checker function

---
+++ main-test.go
@@ -0,0 +1,24 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"os"
+
+// 	// "piscine"
+// 	"strconv"
+// )
+
+// func main() {
+// 	if len(os.Args) != 3 {
+// 		return
+// 	}
+
+// 	x, err1 := strconv.Atoi(os.Args[1])
+// 	y, err2 := strconv.Atoi(os.Args[2])
+
+// 	if err1 != nil || err2 != nil {
+// 		return
+// 	}
+
+// 	fmt.Print(QuadE(x, y))
+// }
